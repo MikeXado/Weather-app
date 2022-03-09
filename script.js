7afe52793ffcd5b1cfe82b439111c57d3babf450
Gurin Mihail <mike.gurin21@gmail.com>
2022-03-09 18:33:55 +0300

    version with table

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
-  var api = `https://api.weatherapi.com/v1/current.json?key=13001c2bc9a841669a8154010220603&q=`
+  var api = `https://api.weatherapi.com/v1/forecast.json?key=13001c2bc9a841669a8154010220603&q=`
   var city = document.querySelector("#locationIn");
-  var apiKey = `&aqi=no`;
+  var apiKey = `&days=1&aqi=no&alerts=no`;
 
   var img = document.createElement("img");
   let div = document.querySelector(".weather-img");
   div.appendChild(img)
-
+  
+  const times = document.querySelectorAll("time").innerHTML;
 
 
 function getApi () {
@@ -18,31 +19,58 @@ function getApi () {
      return response.json();
  })
  .then((data) =>{
-     const {temp_c, pressure_mb , humidity , wind_kph} = data.current;
+     const {temp_c, precip_mm , humidity , wind_kph} = data.current;
      const {icon}  = data.current.condition;
      //SET DOM ELEMENTS 
-    
-    
      data.location.name = document.querySelector("#locationIn").value;
     document.querySelector(".city").textContent = data.location.name;
     img.src = icon ;
     document.querySelector(".temp").textContent = Math.floor(temp_c);
-    
     document.querySelector(".summary-text").textContent = data.current.condition.text;
     document.querySelector(".hu-value").textContent = humidity + "%";
-    document.querySelector(".pressure").textContent = pressure_mb + "mb";
+    document.querySelector(".pressure").textContent = Math.floor(precip_mm) + "mm";
     document.querySelector(".wind").textContent = wind_kph + "kph";
-   
+    
+    
  })
+    document.querySelector(".loader").style.display = "none"
+    document.querySelector(".weather-info__content").style.display = "flex"
 }
-
-getApi()
+getApi();
 
 document.addEventListener("keyup" , (e) => {
     if (e.keyCode === 13) {
-      getApi();
+
+       document.querySelector(".weather-info__content").style.display = "none";
+       document.querySelector(".loader").style.display = "block";
+       document.querySelector(".learn-more").classList.add("hov");
+       document.querySelector(".learn-more").focus();
+       setTimeout(() => {
+        getApi();
+      }, 1000);
     }
-})
+  });
+
+  document.querySelector(".learn-more").addEventListener("click" , ()=>{
+    document.querySelector(".weather-info__content").style.display = "none";
+    document.querySelector(".loader").style.display = "block";
+    document.querySelector(".learn-more").classList.add("hov");
+    setTimeout(() => {
+     getApi();
+   }, 1000);
+  }); 
+
+
+//Table
+$(window).on("load resize ", function() {
+  var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+  $('.tbl-header').css({'padding-right':scrollWidth});
+}).resize();
+
+
+
+//TWO PLACEHOLDERS 
+
 
 
 
