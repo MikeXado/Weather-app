5f6403415dd65acbb89ab8b8e17e5a8b68c4e851
missshanya21 <mike.gurin21@gmail.com>
2022-03-18 22:40:50 +0200

    added exchange currency

--- script.js
+++ script.js
@@ -92,6 +92,91 @@ document.querySelector(".learn-more").addEventListener("click", () => {
 
 
 
+//!CURRENCY FUNCTIONS
+
+
+
+const dropList = document.querySelectorAll("form select"),
+fromCurrency = document.querySelector(".from select"),
+toCurrency = document.querySelector(".to select"),
+getButton = document.querySelector(".button-52");
+for (let i = 0; i < dropList.length; i++) {
+    for(let currency_code in country_list){
+      let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "MDL" ? "selected" : "";
+        let optionTag = `<option value="${currency_code}"${selected}>${currency_code}</option>`;
+        dropList[i].insertAdjacentHTML("beforeend", optionTag);
+    }
+    dropList[i].addEventListener("change", e =>{
+        loadFlag(e.target);
+    });
+}
+function loadFlag(element){
+    for(let code in country_list){
+        if(code == element.value){
+            let imgTag = element.parentElement.querySelector("img");
+            imgTag.src = `https://flagcdn.com/48x36/${country_list[code].toLowerCase()}.png`;
+        }
+    }
+}
+window.addEventListener("load", ()=>{
+    getExchangeRate();
+});
+getButton.addEventListener("click", e =>{
+    e.preventDefault();
+    getExchangeRate();
+});
+const exchangeIcon = document.querySelector("form .icon");
+exchangeIcon.addEventListener("click", ()=>{
+    let tempCode = fromCurrency.value;
+    fromCurrency.value = toCurrency.value;
+    toCurrency.value = tempCode;
+    loadFlag(fromCurrency);
+    loadFlag(toCurrency);
+    getExchangeRate();
+})
+function getExchangeRate(){
+    const amount = document.querySelector("form input");
+    const exchangeRateTxt = document.querySelector("form #converted");
+    let amountVal = amount.value;
+    if(amountVal == "" || amountVal == "0"){
+        amount.value = "1";
+        amountVal = 1;
+    }
+    let url = `https://v6.exchangerate-api.com/v6/9cf651a1b4d7f96a304a4c5a/latest/${fromCurrency.value}`;
+    fetch(url).then(response => response.json()).then(result =>{
+        let exchangeRate = result.conversion_rates[toCurrency.value];
+        let totalExRate = (amountVal * exchangeRate).toFixed(2);
+        exchangeRateTxt.value = totalExRate + " " +  toCurrency.value;
+       
+    }).catch(() =>{
+        console.log("Something went wrong");
+    });
+
+}
+
+
+
+
+//!CLOSE BUTTON 
+  document.querySelector(".close-button").addEventListener("click" , ()=> {
+          document.querySelector(".converter__content").classList.add("converter-close");
+  })
+
+
+  //!CONVERTER CLOSE 
+
+  document.querySelector(".converter__button").addEventListener("click" , ()=> {
+    document.querySelector(".converter__content").classList.toggle("converter-close")
+  })
+
+
+
+
+
+
+
+
+
 
 
 
